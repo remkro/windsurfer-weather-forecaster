34acebf889daac33fb06137e143ba69416a61a12
Remigiusz Królak <krolak.remek@gmail.com>
2023-02-01 14:40:52 +0100

    Created basic project structure

---
+++ src/main/java/com/krolak/windsurferweatherforecaster/WindsurferWeatherForecasterApplication.java
@@ -0,0 +1,13 @@
+package com.krolak.windsurferweatherforecaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindsurferWeatherForecasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindsurferWeatherForecasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krolak/windsurferweatherforecaster/WindsurferWeatherForecasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krolak.windsurferweatherforecaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindsurferWeatherForecasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
