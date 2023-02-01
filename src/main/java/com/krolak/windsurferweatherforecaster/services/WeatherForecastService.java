f40e43bc882a150c948630cb52ec215915adff86
Remigiusz Królak <krolak.remek@gmail.com>
2023-02-01 17:12:08 +0100

    Implemented weather check for multiple locations provided in application.properties

--- src/main/java/com/krolak/windsurferweatherforecaster/services/WeatherForecastService.java
+++ src/main/java/com/krolak/windsurferweatherforecaster/services/WeatherForecastService.java
@@ -1,10 +1,13 @@
 package com.krolak.windsurferweatherforecaster.services;
 
 import com.krolak.windsurferweatherforecaster.dtos.WeatherResponseDto;
+import com.krolak.windsurferweatherforecaster.exceptions.NoLocationsProvidedException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class WeatherForecastService {
@@ -13,10 +16,28 @@ public class WeatherForecastService {
     private String apiKey;
     @Value("${apiUrl}")
     private String apiUrl;
+    @Value("#{'${listOfCities}'.split(',')}")
+    private List<String> locations;
 
-    public String askForWeather() {
-        String resource = apiUrl + "Jastarnia" + "&key=" + apiKey;
-        WeatherResponseDto response = restTemplate.getForObject(resource, WeatherResponseDto.class);
-        return response.toString();
+    public String performForecast() {
+        List<WeatherResponseDto> responses = callWeatherBitApiToCheckWeatherForAllLocations();
+        return "good weather";
+    }
+
+    private List<WeatherResponseDto> callWeatherBitApiToCheckWeatherForAllLocations() {
+        if (locations.size() == 0)
+            throw new NoLocationsProvidedException("There are no locations to check!");
+
+        String resource;
+        WeatherResponseDto response;
+        List<WeatherResponseDto> responses = new ArrayList<>();
+
+        for (String location : locations) {
+            resource = apiUrl + location + "&key=" + apiKey;
+            response = restTemplate.getForObject(resource, WeatherResponseDto.class);
+            responses.add(response);
+        }
+
+        return responses;
     }
 }
