e0e86c3e5ed571ba29a06f7de9091e8b8040e6cb
Remigiusz Królak <krolak.remek@gmail.com>
2023-02-01 16:04:51 +0100

    Introduced service connecting to WeatherBit api, added Lombok

--- src/main/java/com/krolak/windsurferweatherforecaster/controllers/ForecastController.java
+++ src/main/java/com/krolak/windsurferweatherforecaster/controllers/ForecastController.java
@@ -1,9 +1,21 @@
 package com.krolak.windsurferweatherforecaster.controllers;
 
+import com.krolak.windsurferweatherforecaster.services.WeatherForecastService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/forecast")
+@RequiredArgsConstructor
 public class ForecastController {
+    private final WeatherForecastService weatherForecastService;
+
+    @GetMapping
+    public ResponseEntity<String> getForecast() {
+        String response = weatherForecastService.askForWeather();
+        return ResponseEntity.ok(response);
+    }
 }
