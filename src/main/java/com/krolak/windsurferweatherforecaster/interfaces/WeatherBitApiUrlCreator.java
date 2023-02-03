51c00ade943615d7664b0cc5a3194f3f81d7f586
Remigiusz Królak <krolak.remek@gmail.com>
2023-02-03 13:32:07 +0100

    Removed unused import

--- src/main/java/com/krolak/windsurferweatherforecaster/interfaces/WeatherBitApiUrlCreator.java
+++ src/main/java/com/krolak/windsurferweatherforecaster/interfaces/WeatherBitApiUrlCreator.java
@@ -1,7 +1,6 @@
 package com.krolak.windsurferweatherforecaster.interfaces;
 
 import java.util.List;
-import java.util.Map;
 
 public interface WeatherBitApiUrlCreator {
     String create(String city, List<String> coordinates);
